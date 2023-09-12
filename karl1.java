17e6ea09121356549f0aef5ed2c3da71310f32e1
Karl Robert Masing <104931173+Vonnuu@users.noreply.github.com>
2023-09-12 10:48:21 +0300

    Add files via upload

---
+++ karl1.java
@@ -0,0 +1,12 @@
+import javax.swing.text.Style;
+
+public class karl1 {
+    public static void main(String[] args) {
+        
+        String nimi="Karl Robert Masing";
+        int vanus = 17;
+        double pikkus=1.85;
+        String hnimi="Karlito Papito";
+        System.out.println(nimi+", "+vanus+", "+pikkus+"m, "+hnimi+". "+"Tere mina olen "+nimi+" ma olen "+vanus+" aastat vana ja "+pikkus+"m pikk ning mind kutsutakse "+hnimi+"ks");
+    }
+}
