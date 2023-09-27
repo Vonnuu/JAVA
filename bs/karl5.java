d2312ff526639fbd57fd4ffcc8fdf7ac2419cc88
Karl Robert Masing <104931173+Vonnuu@users.noreply.github.com>
2023-09-27 19:20:16 +0300

    Update karl5.java

--- bs/karl5.java
+++ bs/karl5.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
 
 public class karl5 {
     public static void main(String[] args){
-        arvutaja();
-        
+        calc();
     }
-    public static void arvutaja(){
+    public static void calc(){
         for (int i = 0; i < 3; i++){
             Scanner scan = new Scanner(System.in);
             System.out.println("Sisesta esimene arv: ");
             double arv1 = scan.nextInt();
             System.out.println("Sisesta teine arv: ");
             double arv2 = scan.nextInt();
+
             
             if (arv1 <= 0 || arv2 <= 0) {
-                System.out.println("Arv ei jagu");
+                System.out.println("Arvud ei jagu");
                 System.exit(0); 
             }
-            System.out.println("Vastus: " + arv1/arv2);
+            System.out.println("Vastus on: " + arv1/arv2);
             scan.close();
         }
     }
