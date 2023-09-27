599746f0091baa51800576e3a63749de32964762
Karl Robert Masing <104931173+Vonnuu@users.noreply.github.com>
2023-09-27 19:11:40 +0300

    Update Karl3.java

--- bs/Karl3.java
+++ bs/Karl3.java
@@ -3,8 +3,7 @@ public class Karl3 {
         printArray();
         printTemp();
     }
-
-    public static void printArray(){
+    public static void printrida(){
         int summa, pikkus;
         int[] numbrid = {12,5,10,10,28,17,5,19,0,13,0,22,7,0,17,2,24,1,13,29,0,7,16,8,7,4,27,8,8,23,26,25,15,2,26,1};
         pikkus = numbrid.length;
@@ -14,10 +13,9 @@ public class Karl3 {
         }
         double average = summa / pikkus;
         System.out.println("esimene number: " + numbrid[0] + " viimane number: " + numbrid[pikkus-1] + " massivi suurus: " + pikkus + " massivi summa: " + summa + " arvude keskmine: "  + average);
-
         }
     
-    public static void printTemp(){
+    public static void printtemperatuur(){
         int[][] mitmeline = {{1,23},{2,15},{3,29},{4,15},{5,26},{6,17},{7,26},{8,15},{9,28},{10,12},{11,24},{12,16},{13,21},{14,10},{15,10},{16,26},{17,27},{18,19},{19,14},{20,14},{21,14},{22,26},{23,20},{24,28},{25,29},{26,11},{27,28},{28,19},{29,25},{30,12}};
         for (int i = 0; i < mitmeline.length; i++) {
             System.out.println(" Tempid: " + mitmeline[i][1]);
