54260dc64e03b9ab1e11a1955051ce13ef8ced8d
Karl Robert Masing <104931173+Vonnuu@users.noreply.github.com>
2023-09-27 19:18:18 +0300

    Update karl2.java

--- bs/karl2.java
+++ bs/karl2.java
@@ -1,43 +1,35 @@
 public class karl2 {
     public static void main(String args[]) {
-        tollid_meetriteks(7);
-        ellipsi_pindala(85,46);
-        minut_tund(519);
-        double[] list = sportlased(23, 67, 52, 78);
-        sportlanevahe(list[0],list[1]);
+        tollid(89);
+        ellips(5,4);
+        tund(395);
+        double[] list = sport(23, 67, 52, 78);
+        sportlastevahe(list[0],list[1]);
     }
-
-    public static void tollid_meetriteks(double toll) {
-        double meetrid = toll * 0.0254;
-        System.out.println("Tollid meetriteks: " + meetrid);
+    public static void tollid(double toll) {
+        double meetrid= toll * 0.0254;
+        System.out.println("Tollid meetrites: " + meetrid);
     }
-
-    public static void ellipsi_pindala(double a, double b) {
-        double pindala = Math.PI * a * b;
+    public static void ellips(double a, double b) {
+        double pindala= Math.PI * a * b;
         pindala = Math.round(pindala*100)/100;
-        System.out.println("Ellipsi pindala: " + pindala);
+        System.out.println("Ellipsi pindala on : " + pindala);
     }
-
-    public static void minut_tund(double minute) {
-        double tund = minute / 60;
-        double minutes = minute % 60;
-        System.out.println("Minutid tundideks: " + tund + " ja minutid: " + minutes + "");
+    public static void tund(double minute) {
+        double tund= minute / 60;
+        double minut= minute % 60;
+        System.out.println("tunnid: " + tund + " ja minutid: " + minut + "");
     }
-
-    public static double[] sportlased(int km1, int h1, int km2, int h2) {  
-        double sportlane1 = km1 / h1;
-        double sportlane2 = km2 / h2;
-        System.out.println("Sportlane 1: " + sportlane1 + " km/h");
-        System.out.println("Sportlane 2: " + sportlane2 + " km/h");
-        double[] mewmew = {sportlane1, sportlane2};
-        return mewmew;
+    public static double[] sport(int km, int v, int km1, int v2) {  
+        double sport= km / v;
+        double sport1= km1 / v2;
+        System.out.println("Sportlane 1 jookseb: "+sport+"km/h");
+        System.out.println("Sportlane 2 jookseb: "+sport1+"km/h");
+        double[] midagi= {sport, sport1};
+        return midagi;
     }
-
-    public static void sportlanevahe(double sport1, double sport2) {
-        double vahe = sport1%sport2;
-        System.out.println("sportlaste vahe: " + vahe + " km/h");
+    public static void sportlastevahe(double sport2, double sport3) {
+        double vahe = sport2%sport3;
+        System.out.println("sportlaste vahe on: "+vahe+"km/h");
     }
-
-
-
 }
