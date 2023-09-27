3acc993955da376d8db27cab0ab3c09730c9b442
Karl Robert Masing <104931173+Vonnuu@users.noreply.github.com>
2023-09-27 18:59:10 +0300

    Update Lind.java

--- bs/karl10/Lind.java
+++ bs/karl10/Lind.java
@@ -1,26 +1,21 @@
 package karl10;
 
 public class Lind extends Loom{
-    // omadus
 	private int jalaarv;
-	// kontstruktor
 	public Lind(int jalaarv) {
 		this.jalaarv = jalaarv;
 	}
-	// getter
 	public int getjalaarv() {
 		return jalaarv;
 	}
-	// setter
 	public void setjalaarv(int jalaarv) {
 		this.jalaarv = jalaarv;
 	}
-	// meetod
 	public void lenda(){
-		System.out.println("flap flap");
+		System.out.println("lap lap lap lap lap");
 	}
     @Override
     public void toit(){
-        System.out.println("Lind sööb putukaid");
+        System.out.println("Lind sööb saaski");
     }
 }
